3a75b7539b87012c72a8d8667cca1650e4f3aa70
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2023-08-09 17:45:00 +0000

    Deploying to gh-pages from @ Ferst1/goit-js-hw-09@004b97cc0bed4b0aef82043e1f14ed277ce61a53 🚀

---
+++ 01-color-switcher.336ded8d.js
@@ -0,0 +1,2 @@
+document.querySelectorAll;
+//# sourceMappingURL=01-color-switcher.336ded8d.js.map
---
+++ 01-color-switcher.761596a1.js
@@ -0,0 +1,2 @@
+document.querySelectorAll;
+//# sourceMappingURL=01-color-switcher.761596a1.js.map
