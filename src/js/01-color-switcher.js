b6cc9e87f05009b68acf4ef99011e9bdeaea6086
Ferst1 <helenochka4@gmail.com>
2023-08-09 20:53:52 +0200

    task1
    updatei task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
-let button = document.querySelectorAll;
+let intervalId = null;
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', startColorChanging);
+stopButton.addEventListener('click', stopColorChanging);
+
+function startColorChanging() {
+  if (intervalId === null) {
+    intervalId = setInterval(changeBackgroundColor, 1000);
+  }
+}
+
+function stopColorChanging() {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
