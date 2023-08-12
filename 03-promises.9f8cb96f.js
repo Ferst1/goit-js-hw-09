13c2ee59f79ef3ff9f48c6775c064342cd6b6446
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2023-08-12 20:24:24 +0000

    Deploying to gh-pages from @ Ferst1/goit-js-hw-09@b7dfc7539db7b888abfa48dd1eb949de517e9ec5 🚀

--- 03-promises.9f8cb96f.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("showButton").addEventListener("click",(function(){Notiflix.Notify.success("Уведомление успешно отображено!")}));
-//# sourceMappingURL=03-promises.9f8cb96f.js.map
--- 03-promises.e3179f08.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("showButton").addEventListener("click",(function(){Notiflix.Notify.success("Уведомление успешно отображено!")}));
-//# sourceMappingURL=03-promises.e3179f08.js.map
